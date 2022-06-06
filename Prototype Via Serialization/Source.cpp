26214c4261ddfd26c10a7ba50f35c576aa770590
iDividedBy0 <ttroczynski3063@conestogac.on.ca>
2022-06-05 22:51:15 -0400

    Update

--- Prototype Via Serialization/Source.cpp
+++ Prototype Via Serialization/Source.cpp
@@ -9,14 +9,21 @@
 #include <sstream>
 #include <memory>
 #include <cmath>
-
+#include <boost/serialization/serialization.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
 
 using namespace std;
+
+using namespace boost;
+
 struct Address
 {
 	string street, city;
 	int suite;
 
+
+	Address() {}
 	Address(const string& street, const string& city, int suite) : street(street), city(city), suite(suite) {}
 	Address(Address& address) : street(address.street), city(address.city), suite(address.suite) {}
 
@@ -24,6 +31,17 @@ struct Address
 	{
 		os << "street: " << address.street << " city: " << address.city << " suite: " << address.suite << endl;
 		return os;
+	}
+
+private:
+	friend class serialization::access;
+
+	template<class archive>
+	void serialize(archive& ar, const unsigned version)
+	{
+		ar& street;
+		ar& city;
+		ar& suite;
 	}
 
 };
@@ -34,15 +52,29 @@ struct Contact
 	string name;
 	Address* address;
 
+	Contact() {}
+
 	Contact(const string& name, Address* address) : name(name), address(address) {}
 
 	Contact(const Contact& other) : name{ other.name }, address{ new Address {*other.address} } {}
+
+	~Contact() { delete address; }
 
 	friend ostream& operator<< (ostream& os, Contact& contact)
 	{
 		os << "name: " << contact.name << endl;
 		os << *contact.address;
 		return os;
+	}
+
+private:
+	friend class serialization::access;
+
+	template<class archive>
+	void serialize(archive& ar, const unsigned version)
+	{
+		ar& name;
+		ar& address;
 	}
 };
 
@@ -70,10 +102,26 @@ private:
 
 int main()
 {
+	auto clone = [](const Contact& c)
+	{
+		ostringstream oss;
+		archive::text_oarchive oa(oss);
+		oa << c;
 
-	auto john = EmployeeFactory::new_main_office_employee("john", 123);
+		string s = oss.str();
+		cout << s << endl;
 
-	cout << *john << endl;
+		istringstream iss(s);
+		archive::text_iarchive ia(iss);
+
+		Contact result;
+		ia >> result;
+
+		return result;
+	};
+
+	auto john = EmployeeFactory::new_main_office_employee("John", 123);
+	auto jane = clone(*john);
 
 	return 0;
 
